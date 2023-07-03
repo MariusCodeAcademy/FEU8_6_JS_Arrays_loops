112689633039b37b0d000f74a91f24b8ff760f08
Marius CA <mariuskcodeacademy@gmail.com>
2023-07-03 09:51:53 +0300

    spread

---
+++ js/spread.js
@@ -0,0 +1,6 @@
+'use strict';
+console.log('spread.js file was loaded');
+
+// idx               0          1            2        3
+const weekDays = ['monday', 'wednesday', 'friday', 'sunday'];
+console.log('weekDays ===', weekDays);
