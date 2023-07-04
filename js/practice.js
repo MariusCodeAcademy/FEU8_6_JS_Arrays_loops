8d309f3a6b0479af561911417563584a956823b1
Marius CA <mariuskcodeacademy@gmail.com>
2023-07-04 08:41:44 +0300

    psiaudo

---
+++ js/practice.js
@@ -0,0 +1,25 @@
+'use strict';
+console.log('practice.js file was loaded');
+
+const numsArr = [2, 12, 33, -7, 28, 49, -17, 19, 42, -14, -10, 20];
+
+// Sukurti funkciją, kuri ima masyvą ir grąžina didžiau-
+//sią skaičių masyve.
+
+// Psiaudo kodas
+
+// sukti cikla per visa masyva gaunant kiiekviena reiksme
+// lygininti kiekviena reiksme ir ziureti kuri mazesne.
+// lyginti su paciu pirmu elementu.
+// jei randam mazesni, jis tampa minimaliu
+// po ciklu minimalus yra minimumas
+
+/*
+let minimalus = <pirmam masyvo elementui>
+for (<ciklas per masyva>) {
+<ar minimalus yra mazesnis uz dabartini elementa> ?
+  taip - minimalus lieka tas pats
+  ne - dabartinis elementas yra naujas minimalus
+}
+
+*/
